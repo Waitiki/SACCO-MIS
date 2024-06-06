042134aaff774af8ab77e10fd9893de4566bfc6e
Waitiki <victorwainana4@gmail.com>
2024-06-06 07:13:59 -0400

    Updates

---
+++ assets/popMenu-d1ca3591.js
@@ -0,0 +1,1 @@
+import{_ as t,o as a,c,b as e,t as o}from"./index-8a6f2ca1.js";const _={props:{message:String},data(){return{}}},n={class:"space-wrap"},p={class:"popeSpace"},r={class:"message-space"};function d(i,l,s,u,f,m){return a(),c("div",n,[e("div",p,[e("div",r,[e("h2",null,o(s.message),1)])])])}const v=t(_,[["render",d],["__scopeId","data-v-c2f59e41"]]);export{v as p};
